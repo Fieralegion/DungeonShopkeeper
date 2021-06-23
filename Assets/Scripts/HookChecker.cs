6a2a0263119da7cd63fa26e93a43b842510e06e5
DESKTOP-OILDAUE\maxim <maxisanturba@gmail.com>
2021-06-23 19:01:00 +0200

    no message

--- Assets/Scripts/HookChecker.cs
+++ Assets/Scripts/HookChecker.cs
@@ -4,4 +4,10 @@ using UnityEngine;
 
 public class HookChecker : MonoBehaviour
 {
+    // guardar historial de items 
+    // tener info del item actual
+    // diferenciar entre los ganchos de enfrente y los de atras.
+
+    // si coloco un objecto en un gancho y lo quiero cambiar por otro item? como realizarlo
 }
+    // si el objeto que recibe el cliente es incorrecto entonces este item se regresa al gancho
